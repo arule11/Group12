681be4b3a12c084f33aef76b6dc9672b3afc0b36
arule11 <47372331+arule11@users.noreply.github.com>
2019-02-28 12:09:28 -0700

    Add files via upload
    i changed the rows and columns cause they were backwards

--- Ship.java
+++ Ship.java
@@ -1,78 +1,39 @@
-/**
- * Class for the Ship object. Ships have a name, length, position on the board, and orientation. They can take hits
- * and be sunk. 
- * @author Liam Langill
- * Code used as a reference: https://codereview.stackexchange.com/questions/161680/oop-battleship-console-game-in-java/161695
- *  
- *
- */
+
+
 public class Ship {
-	
-	private String name;
-	private int length;
-	private int shipXcoord;
-	private int shipYcoord;
-	private boolean isSunk;
-	private int hitsTaken;
-	private boolean orientation;
-	
-	public Ship(String name, int length) {
-		this.name = name;
-		this.length = length;
-		this.isSunk = false;
-		this.hitsTaken = 0;
+
+	public int length;
+	public int row;
+	public int column;
+	public char direction;
+
+	public Ship() {
 
 	}
-	
-	public String getName() {
-		return name;
+	public Ship(int length) {
+		this.length = length;
 	}
-	
-	public int getLength() {
+
+
+	public int getRow() {
+    return row;
+  }
+
+  public int getColumn() {
+    return column;
+  }
+
+  public char getDirection(){
+    return direction;
+  }
+
+	public int getLength(){
 		return length;
 	}
-	
-	public int getHitsTaken() {
-		return hitsTaken;
+
+
+	public String toString(){
+	     return Integer.toString(length) + " " +  (char)(row+65) + " " + Integer.toString(column) + " " + direction;
 	}
-	
-	public int getShipXcoord() {
-		return shipXcoord;
-	}
-	
-	public int getShipYcoord() {
-		return shipYcoord;
-	}
-	
-	public boolean getOrientation() {
-		return orientation;
-	}
-	
-	public boolean getIsSunk() {
-		return isSunk;
-	}
-	
-	
-	/**
-	 * 
-	 * @param shipXcoord The X coordinate of the bottom or left edge of the ship being placed
-	 * @param shipYcoord The Y coordinate of the bottom or left edge of the ship being placed
-	 */
-	public void setLocation(int inXcoord, int inYcoord) {
-		this.shipXcoord = inXcoord;
-		this.shipYcoord = inYcoord;
-	} 
-	
-	/**
-	 * When a ship is hit, this method checks if the ship can take any more hits.
-	 * If it cannot take any more hits, the ship is sunk.
-	 */
-	public void shipWasHit() {
-		hitsTaken = hitsTaken + 1;
-		System.out.println("You hit the " + name);
-		if(hitsTaken == length) {
-			isSunk = true;
-			System.out.println("You sunk the " + name);
-		}
-	}
+
 }
---
+++ Site.java
@@ -0,0 +1,34 @@
+
+public class Site {
+
+	public static final String shipSite = "+";
+	public static final String noShip = "-" ;
+	public static final String hit = "1";
+	public static final String miss = "0";
+		
+	public int status;
+	
+	public Site() {
+		this.status = 0;
+	}
+	public void siteMark() {
+		if (status == 0) {
+			System.out.print(noShip);
+		} else if (status == 1) {
+			System.out.print(shipSite);
+		} else if (status == 2) {
+			System.out.print(hit);
+		} else {
+			System.out.print(miss);
+		}
+	}
+	
+	
+	//setHit
+	
+//	setMiss
+	
+//	setShip
+	
+	
+}
