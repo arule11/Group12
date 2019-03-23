6fa71e112aa816afed19486e6f8a398f3976a9a5
KR Novak <47372532+krnovak@users.noreply.github.com>
2019-03-23 15:36:31 -0600

    Update Board.java

--- gui2/Board.java
+++ gui2/Board.java
@@ -1,4 +1,11 @@
+// Thie version of Battleship was written with reference to Java-Battleship
+// by Yuval Marcus (github: ymarcus93)
 
+/**
+* Class representing a Board. sunkShips is an int and the board is a
+* list of Sites. Ships can be placed onto the board and the board can
+* be shown.
+*/
 
 public class Board {
 
@@ -6,7 +13,9 @@ public class Board {
 	public Site[][] board = new Site[10][10];
 	public int sunkShips = 0;
 
-
+	/**
+	* Default constructor: Initializes a 10 by 10 game board
+	*/
 	public Board() {
 		 for(int row = 0 ; row < 10 ; row++ ){
 			 for(int column = 0 ; column < 10 ; column++ ){
@@ -16,7 +25,9 @@ public class Board {
 			 }
 		 }
 	}
-	
+	/**
+	* displays the game board
+	*/
 	public void showBoard(){
 		System.out.println("\n\tA \tB \tC \tD \tE \tF \tG \tH \tI \tJ");
         System.out.println();
@@ -30,7 +41,13 @@ public class Board {
             System.out.println();
         }
 	}
-	
+
+	/**
+	* Checks that the spot the player wishes to place their ship is not
+	* already occupied
+	* @param ship : the ship the play is placing on the board
+	* @return Returns a boolean
+	*/
 	public boolean freeSpace(Ship ship) {
 		if (!ship.inBounds()) {
 			return false;
@@ -51,7 +68,11 @@ public class Board {
 		return true;
 		}
 	}
-	
+
+	/**
+	* Adds the specified ship to the players board
+	* @param ship : the ship the play is placing on the board
+	*/
 	public void addShip(Ship ship) {
 		if (ship.direction == 'H'){ // Horizontal
             for (int i = ship.column; i < ship.column+ship.shipLength; i++){
@@ -62,8 +83,6 @@ public class Board {
             	board[i][ship.column].status = 1;
             }
         }
-		
     }
-
 
 }
